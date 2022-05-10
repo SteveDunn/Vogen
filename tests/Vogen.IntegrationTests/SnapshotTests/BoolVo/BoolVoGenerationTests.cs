c9467587a29d9a53119e33705af4635db6e93ede
Steve Dunn <steve@dunnhq.com>
2022-05-10 06:27:10 +0100

    Move test types. Parameterize generation tests.

--- tests/Vogen.IntegrationTests/SnapshotTests/BoolVo/BoolVoGenerationTests.cs
+++ tests/Vogen.IntegrationTests/SnapshotTests/BoolVo/BoolVoGenerationTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using FluentAssertions;
 using VerifyTests;
 using VerifyXunit;
@@ -9,11 +11,84 @@ namespace Vogen.IntegrationTests.SnapshotTests.BoolVo;
 [UsesVerify] 
 public class BoolVoGenerationTests
 {
-    [Fact]
-    public Task NoConverterBoolVo_Test() =>
-        RunTest(@"
-  [ValueObject(conversions: Conversions.None, underlyingType: typeof(bool))]
-  public partial struct NoConverterBoolVo { }");
+    public class Types : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            foreach (string type in _types)
+            {
+                foreach (string conversion in _conversions)
+                {
+                    foreach (string underlyingType in _underlyingTypes)
+                    {
+                        yield return new object[] { type, conversion, underlyingType, createClassName(type, conversion, underlyingType) };
+                    }
+                }
+            }
+        }
+
+        private string createClassName(string type, string conversion, string underlyingType) => 
+            type.Replace(" ", "_") + conversion.Replace(".", "_") + underlyingType;
+
+        private readonly string[] _types = new[]
+        {
+            "public partial struct",
+            "public partial class",
+            "public partial record struct",
+            "public partial record class",
+            "public readonly partial struct",
+            "public readonly partial record struct"
+        };
+
+        // for each of the types above, create classes for each one of these attributes
+        private readonly string[] _conversions = new[]
+        {
+            "Conversions.None",
+            "Conversions.TypeConverter",
+            "Conversions.NewtonsoftJson",
+            "Conversions.SystemTextJson",
+            "Conversions.NewtonsoftJson | Conversions.SystemTextJson",
+            "Conversions.EfCoreValueConverter",
+            "Conversions.DapperTypeHandler",
+            "Conversions.LinqToDbValueConverter",
+        };
+
+        // for each of the attributes above, use this underlying type
+        private readonly string[] _underlyingTypes = new[]
+        {
+            "bool",
+            "string",
+            "int",
+            "char",
+            "double",
+            "float"
+        };
+        
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    [Theory]
+    [ClassData(typeof(Types))]
+    public Task NoConverterBoolVo_Test(string type, string conversions, string underlyingType, string className)
+    {
+        string declaration = $@"
+  [ValueObject(conversions: {conversions}, underlyingType: typeof({underlyingType}))]
+  {type} {className} {{ }}";
+        var source = @"using Vogen;
+namespace Whatever
+{
+" + declaration + @"
+}";
+
+        var (diagnostics, output) = TestHelper.GetGeneratedOutput<ValueObjectGenerator>(source);
+
+        diagnostics.Should().BeEmpty();
+
+        VerifySettings settings = new VerifySettings();
+        settings.UseFileName(className);
+        return Verifier.Verify(output, settings).UseDirectory("Snapshots");
+    }
+
 
     [Fact]
     public Task NoJsonBoolVo_Test()
