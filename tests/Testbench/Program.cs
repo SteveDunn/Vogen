e266128843ff21462541b465352771fe9848460b
Steve Dunn <steve@dunnhq.com>
2022-05-11 20:46:20 +0100

    Added more generation tests that exercise different underlying types in different types, and with different conversions

--- tests/Testbench/Program.cs
+++ tests/Testbench/Program.cs
@@ -11,8 +11,8 @@ public class Program
 {
     public static void Main()
     {
-        var s = Score.From(1);
-        Console.WriteLine(s.Value);
+        //var s = Score.From(1);
+        //Console.WriteLine(s.Value);
     }
 }
 
@@ -24,10 +24,14 @@ public class Program
 //     private static Int32 NormalizeInput(int input) => Math.Min(128, input);
 // }
 
-[ValueObject]
-public readonly partial record struct Score
-{
-    private static Validation Validate(int value) => value >= 0 ? Validation.Ok : Validation.Invalid("Score must be zero or more");
-}
+// [ValueObject]
+// public readonly partial record struct Score
+// {
+//     private static Validation Validate(int value) => value >= 0 ? Validation.Ok : Validation.Invalid("Score must be zero or more");
+// }
+
+
+[ValueObject(conversions: Conversions.DapperTypeHandler, underlyingType: typeof(DateTime))]
+public partial class public_partial_classConversions_DapperTypeHandlerDateTime { }
 
 
---
+++ tests/Vogen.IntegrationTests/SnapshotTests/GenerationTests/GenerationTests.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using VerifyTests;
+using VerifyXunit;
+using Xunit;
+
+namespace Vogen.IntegrationTests.SnapshotTests.GenerationTests;
+
+[UsesVerify] 
+public class GenerationTests
+{
+    public class Types : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            foreach (string type in _types)
+            {
+                foreach (string conversion in _conversions)
+                {
+                    foreach (string underlyingType in _underlyingTypes)
+                    {
+                        yield return new object[] { type, conversion, underlyingType, createClassName(type, conversion, underlyingType) };
+                    }
+                }
+            }
+        }
+
+        private string createClassName(string type, string conversion, string underlyingType) => 
+            type.Replace(" ", "_") + conversion.Replace(".", "_").Replace("|", "_") + underlyingType;
+
+        private readonly string[] _types = new[]
+        {
+            "public partial struct",
+            "public partial class",
+            "public partial record struct",
+            "public partial record class",
+            "public readonly partial struct",
+            "public readonly partial record struct"
+        };
+
+        // for each of the types above, create classes for each one of these attributes
+        private readonly string[] _conversions = new[]
+        {
+            "Conversions.None",
+            "Conversions.TypeConverter",
+            "Conversions.NewtonsoftJson",
+            "Conversions.SystemTextJson",
+            "Conversions.NewtonsoftJson | Conversions.SystemTextJson",
+            "Conversions.EfCoreValueConverter",
+            "Conversions.DapperTypeHandler",
+            "Conversions.LinqToDbValueConverter",
+        };
+
+        // for each of the attributes above, use this underlying type
+        private readonly string[] _underlyingTypes = new[]
+        {
+            "byte",
+            "char",
+            "bool",
+            "System.DateTimeOffset",
+            "System.DateTime",
+            "decimal",
+            "double",
+            "float",
+            //"FooVo",
+            "int",
+            "System.Guid",
+            "long",
+            "short",
+            "string",
+        };
+        
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    [Theory]
+    [ClassData(typeof(Types))]
+    public Task GenerationTest(string type, string conversions, string underlyingType, string className)
+    {
+        string declaration = $@"
+  [ValueObject(conversions: {conversions}, underlyingType: typeof({underlyingType}))]
+  {type} {className} {{ }}";
+        var source = @"using Vogen;
+namespace Whatever
+{
+" + declaration + @"
+}";
+
+        var (diagnostics, output) = TestHelper.GetGeneratedOutput<ValueObjectGenerator>(source);
+
+        diagnostics.Should().BeEmpty();
+
+        VerifySettings settings = new VerifySettings();
+        settings.UseFileName(className);
+        return Verifier.Verify(output, settings).UseDirectory("Snapshots");
+    }
+}
