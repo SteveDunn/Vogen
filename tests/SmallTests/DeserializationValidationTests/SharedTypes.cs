920e840e08b31e8bc158d2fc782ef7e6b1fc7b31
bobstanton <2547104+bobstanton@users.noreply.github.com>
2022-06-29 13:16:39 -0400

    Tests for type conversion and deserialization, using int and string as underlying types

---
+++ tests/SmallTests/DeserializationValidationTests/SharedTypes.cs
@@ -0,0 +1,111 @@
+﻿using LinqToDB;
+using LinqToDB.Data;
+using LinqToDB.DataProvider.SQLite;
+using LinqToDB.Mapping;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Vogen;
+
+namespace SmallTests.DeserializationValidationTests;
+
+#region Value Objects
+[ValueObject(typeof(int), Conversions.DapperTypeHandler | Conversions.EfCoreValueConverter | Conversions.LinqToDbValueConverter | Conversions.NewtonsoftJson | Conversions.SystemTextJson | Conversions.TypeConverter)]
+public partial class MyVoInt_should_not_bypass_validation
+{
+    private static Validation validate(int value)
+    {
+        if (value > 0)
+            return Validation.Ok;
+
+        return Validation.Invalid("must be greater than zero");
+    }
+}
+
+[ValueObject(typeof(string), Conversions.DapperTypeHandler | Conversions.EfCoreValueConverter | Conversions.LinqToDbValueConverter | Conversions.NewtonsoftJson | Conversions.SystemTextJson | Conversions.TypeConverter)]
+public partial class MyVoString_should_not_bypass_validation
+{
+    private static Validation validate(string value)
+    {
+        if (value.Length > 10)
+            return Validation.Ok;
+
+        return Validation.Invalid("length must be greater than ten characters");
+    }
+}
+#endregion
+
+#region DBContext
+public class DeserializationValidationDbContext : DbContext
+{
+    public DbSet<DeserializationValidationTestIntEntity>? IntEntities { get; set; }
+    public DbSet<DeserializationValidationTestStringEntity>? StringEntities { get; set; }
+
+
+    public DeserializationValidationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<DeserializationValidationTestIntEntity>(builder =>
+            {
+                builder
+                    .Property(x => x.Id)
+                    .HasConversion(new MyVoInt_should_not_bypass_validation.EfCoreValueConverter())
+                    .ValueGeneratedNever();
+            });
+        modelBuilder
+            .Entity<DeserializationValidationTestStringEntity>(builder =>
+            {
+                builder
+                    .Property(x => x.Id)
+                    .HasConversion(new MyVoString_should_not_bypass_validation.EfCoreValueConverter())
+                    .ValueGeneratedNever();
+            });
+    }
+}
+
+public class DeserializationValidationDataConnection : DataConnection
+{
+    public ITable<DeserializationValidationTestIntEntity> IntEntities => GetTable<DeserializationValidationTestIntEntity>();
+    public ITable<DeserializationValidationTestStringEntity> StringEntities => GetTable<DeserializationValidationTestStringEntity>();
+
+    public DeserializationValidationDataConnection(SqliteConnection connection)
+        : base(
+              SQLiteTools.GetDataProvider("SQLite.MS"),
+              connection,
+              disposeConnection: false)
+    { }
+}
+#endregion
+
+#region Entities
+#region EF
+public class DeserializationValidationTestIntEntity
+{
+    public MyVoInt_should_not_bypass_validation? Id { get; set; }
+}
+
+public class DeserializationValidationTestStringEntity
+{
+    public MyVoString_should_not_bypass_validation? Id { get; set; }
+}
+#endregion
+
+#region LinqToDB
+public class DeserializationValidationTestLinqToDbTestIntEntity
+{
+    [Column(DataType = DataType.Int32)]
+    [ValueConverter(ConverterType = typeof(MyVoInt_should_not_bypass_validation.LinqToDbValueConverter))]
+    public MyVoInt_should_not_bypass_validation? Id { get; set; }
+}
+
+public class DeserializationValidationTestLinqToDbTestStringEntity
+{
+    [Column(DataType = DataType.VarChar)]
+    [ValueConverter(ConverterType = typeof(MyVoString_should_not_bypass_validation.LinqToDbValueConverter))]
+    public MyVoString_should_not_bypass_validation? Id { get; set; }
+}
+#endregion
+#endregion
---
+++ tests/SmallTests/DeserializationValidationTests/StringDeserializationValidationTests.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Data.Sqlite;
+using Xunit;
+using Vogen;
+using Dapper;
+using Newtonsoft.Json;
+using NewtonsoftJsonSerializer = Newtonsoft.Json.JsonConvert;
+using SystemTextJsonSerializer = System.Text.Json.JsonSerializer;
+using Microsoft.EntityFrameworkCore;
+using LinqToDB;
+
+namespace SmallTests.DeserializationValidationTests;
+
+public class StringDeserializationValidationTests
+{
+    public StringDeserializationValidationTests()
+    {
+        SqlMapper.AddTypeHandler(new MyVoString_should_not_bypass_validation.DapperTypeHandler());
+    }
+
+    [Fact]
+    public async void Deserialization_dapper_should_not_bypass_validation_pass()
+    {
+        using var connection = new SqliteConnection("DataSource=:memory:");
+        await connection.OpenAsync();
+
+        var actual = (await connection.QueryAsync<MyVoString_should_not_bypass_validation>("SELECT 'abcdefghijk'")).AsList()[0].Value;
+
+        actual.Should().Be("abcdefghijk");
+    }
+
+    [Fact]
+    public async void Deserialization_dapper_should_not_bypass_validation_fail()
+    {
+        using var connection = new SqliteConnection("DataSource=:memory:");
+        await connection.OpenAsync();
+
+        Func<Task<string>> vo = async () => (await connection.QueryAsync<MyVoString_should_not_bypass_validation>("SELECT 'abc'")).AsList()[0].Value;
+
+        await vo.Should().ThrowExactlyAsync<ValueObjectValidationException>().WithMessage("length must be greater than ten characters");
+    }
+
+    [Fact]
+    public async void Deserialization_efcore_should_not_bypass_validation_pass()
+    {
+        var connection = new SqliteConnection("DataSource=:memory:");
+        await connection.OpenAsync();
+
+        var options = new DbContextOptionsBuilder<DeserializationValidationDbContext>()
+            .UseSqlite(connection)
+            .Options;
+
+        using (var context = new DeserializationValidationDbContext(options))
+        {
+            var actual = await Microsoft.EntityFrameworkCore.EntityFrameworkQueryableExtensions.SingleAsync(context.StringEntities!.FromSqlRaw("SELECT 'abcdefghijk' As Id"));
+            actual.Id!.Value.Should().Be("abcdefghijk");
+        }
+    }
+
+    [Fact]
+    public async void Deserialization_efcore_should_not_bypass_validation_fail()
+    {
+        var connection = new SqliteConnection("DataSource=:memory:");
+        await connection.OpenAsync();
+
+        var options = new DbContextOptionsBuilder<DeserializationValidationDbContext>()
+            .UseSqlite(connection)
+            .Options;
+
+        using (var context = new DeserializationValidationDbContext(options))
+        {
+            Func<Task<string>> vo = async () => (await Microsoft.EntityFrameworkCore.EntityFrameworkQueryableExtensions.SingleAsync(context.StringEntities!.FromSqlRaw("SELECT 'abc' As Id"))).Id!.Value;
+            await vo.Should().ThrowExactlyAsync<ValueObjectValidationException>().WithMessage("length must be greater than ten characters");
+        }
+    }
+    [Fact]
+    public async void Deserialization_linqtodb_should_not_bypass_validation_pass()
+    {
+        var connection = new SqliteConnection("DataSource=:memory:");
+        await connection.OpenAsync();
+
+        using (var context = new DeserializationValidationDataConnection(connection))
+        {
+            var actual = await LinqToDB.AsyncExtensions.SingleAsync(context.FromSql<DeserializationValidationTestLinqToDbTestStringEntity>("SELECT 'abcdefghijk' As Id"));
+            actual.Id!.Value.Should().Be("abcdefghijk");
+        }
+    }
+
+    [Fact]
+    public async void Deserialization_linqtodb_should_not_bypass_validation_fail()
+    {
+        var connection = new SqliteConnection("DataSource=:memory:");
+        await connection.OpenAsync();
+
+        //var original = new TestEntity { Id = LinqToDbStringVo.From("foo!") };
+        using (var context = new DeserializationValidationDataConnection(connection))
+        {
+            Func<Task<string>> vo = async () => (await LinqToDB.AsyncExtensions.SingleAsync(context.FromSql<DeserializationValidationTestLinqToDbTestStringEntity>("SELECT 'abc' As Id"))).Id!.Value;
+            await vo.Should().ThrowExactlyAsync<ValueObjectValidationException>().WithMessage("length must be greater than ten characters");
+        }
+    }
+
+    [Fact]
+    public void TypeConversion_should_not_bypass_validation_pass()
+    {
+        var converter = TypeDescriptor.GetConverter(typeof(MyVoString_should_not_bypass_validation));
+        var validValue = "abcdefghijk";
+
+        var actual = ((MyVoString_should_not_bypass_validation?) converter.ConvertFrom(validValue))!.Value;
+
+        actual.Should().Be("abcdefghijk");
+    }
+
+    [Fact]
+    public void TypeConversion_should_not_bypass_validation_fail()
+    {
+        var converter = TypeDescriptor.GetConverter(typeof(MyVoString_should_not_bypass_validation));
+        var invalidValue = "abc";
+
+        Action vo = () => converter.ConvertFrom(invalidValue);
+
+        vo.Should().ThrowExactly<ValueObjectValidationException>().WithMessage("length must be greater than ten characters");
+    }
+
+    [Fact]
+    public void Deserialization_systemtextjson_should_not_bypass_validation_pass()
+    {
+        var validValue = SystemTextJsonSerializer.Serialize(MyVoString_should_not_bypass_validation.From("abcdefghijk"));
+
+        var actual = SystemTextJsonSerializer.Deserialize<MyVoString_should_not_bypass_validation>(validValue)!.Value;
+
+        actual.Should().Be("abcdefghijk");
+    }
+
+    [Fact]
+    public void Deserialization_systemtextjson_should_not_bypass_validation_fail()
+    {
+        var invalidValue = SystemTextJsonSerializer.Serialize(MyVoString_should_not_bypass_validation.From("abcdefghijk")).Replace("abcdefghijk", "abc");
+
+        Action vo = () => SystemTextJsonSerializer.Deserialize<MyVoString_should_not_bypass_validation>(invalidValue);
+
+        vo.Should().ThrowExactly<ValueObjectValidationException>().WithMessage("length must be greater than ten characters");
+    }
+
+    [Fact]
+    public void Deserialization_newtonsoft_should_not_bypass_validation_pass()
+    {
+        var validValue = NewtonsoftJsonSerializer.SerializeObject(MyVoString_should_not_bypass_validation.From("abcdefghijk"));
+
+        var actual = NewtonsoftJsonSerializer.DeserializeObject<MyVoString_should_not_bypass_validation>(validValue).Value;
+
+        actual.Should().Be("abcdefghijk");
+    }
+
+    [Fact]
+    public void Deserialization_newtonsoft_should_not_bypass_validation_fail()
+    {
+        var invalidValue = NewtonsoftJsonSerializer.SerializeObject(MyVoString_should_not_bypass_validation.From("abcdefghijk")).Replace("abcdefghijk", "abc");
+
+        Func<string> vo = () => NewtonsoftJsonSerializer.DeserializeObject<MyVoString_should_not_bypass_validation>(invalidValue).Value;
+
+        vo.Should().ThrowExactly<ValueObjectValidationException>().WithMessage("length must be greater than ten characters");
+    }
+
+}
