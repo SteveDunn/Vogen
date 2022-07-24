5b0f770f98b68afd2c32dd3a10c568ffd550ff3c
Steve Dunn <steve@dunnhq.com>
2022-07-24 07:22:35 +0100

    Added more tests

---
+++ tests/SmallTests/ToStringTests/BasicFunctionality.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Vogen.Tests.Types;
+using Xunit;
+
+namespace SmallTests.ToStringTests;
+
+public class BasicFunctionality
+{
+    [Fact]
+    public void ToString_uses_generated_method()
+    {
+        Age.From(18).ToString().Should().Be("18");
+        Age.From(100).ToString().Should().Be("100");
+        Age.From(1_000).ToString().Should().Be("1000");
+
+        Name.From("fred").ToString().Should().Be("fred");
+        Name.From("barney").ToString().Should().Be("barney");
+        Name.From("wilma").ToString().Should().Be("wilma");
+    }
+}
---
+++ tests/SmallTests/ToStringTests/Derivation.cs
@@ -0,0 +1,65 @@
+﻿using FluentAssertions;
+using Vogen;
+using Xunit;
+
+namespace SmallTests.ToStringTests;
+
+public class Derivation
+{
+    [Fact]
+    public void ToString_uses_users_method() => 
+        Vo.From(18).ToString().Should().Be("ToString_Vo!");
+
+    [Fact]
+    public void ToString_uses_derived_method() => 
+        Vod1.From(18).ToString().Should().Be("derived1!");
+
+    [Fact]
+    public void ToString_uses_least_derived_method() => 
+        Vod2.From(18).ToString().Should().Be("derived2!");
+
+    [Fact]
+    public void ToString_on_a_record_uses_least_derived_method() => 
+        Vor1.From(18).ToString().Should().Be("Vor1!");
+}
+
+public class D1
+{
+    public override string ToString() => "derived1!";
+}
+
+public class D2 : D1
+{
+    public new virtual string ToString() => "derived2!";
+}
+
+public class D3 : D2
+{
+}
+
+[ValueObject]
+public partial class Vo
+{
+    public override string ToString() => "ToString_Vo!";
+}
+
+[ValueObject]
+public partial class Vod1 : D1
+{
+}
+
+[ValueObject]
+public partial class Vod2 : D3
+{
+}
+
+public record R1
+{
+    public override string ToString() => "R1!";
+}
+
+[ValueObject]
+public partial record Vor1 : R1
+{
+    public override sealed string ToString() => "Vor1!";
+}
