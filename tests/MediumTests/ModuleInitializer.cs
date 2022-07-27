fed2d4c9fbe7fd7b890ad693e343626b55634317
Steve Dunn <steve@dunnhq.com>
2022-07-27 06:48:57 +0100

    fix tests

--- tests/MediumTests/ModuleInitializer.cs
+++ tests/MediumTests/ModuleInitializer.cs
@@ -12,7 +12,7 @@ public static class ModuleInitializer
         VerifySourceGenerators.Enable();
         
         VerifierSettings.AddScrubber(s =>
-            s.Replace("    [global::System.CodeDom.Compiler.GeneratedCodeAttribute(\"Vogen\", \"0.0.0.0\")]",
+            s.Replace("    [global::System.CodeDom.Compiler.GeneratedCodeAttribute(\"Vogen\", \"2.0.0.0\")]",
                 "    [global::System.CodeDom.Compiler.GeneratedCodeAttribute(\"Vogen\", \"1.0.0.0\")]"));
 
         SqlMapper.AddTypeHandler(new Vogen.IntegrationTests.TestTypes.ClassVos.DapperFooVo.DapperTypeHandler());
