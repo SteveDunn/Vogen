6723d4fed4ed5f0a3bc192b5946edcdabd164987
Steve Dunn <steve@dunnhq.com>
2022-07-31 06:27:37 +0100

    Move tests over

---
+++ src/Vogen/Analyzers/Analyzer1Analyzer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Vogen.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class Analyzer1Analyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "Analyzer1";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle),
+            Resources.ResourceManager, typeof(Resources));
+
+        private static readonly LocalizableString MessageFormat =
+            new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager,
+                typeof(Resources));
+
+        private static readonly LocalizableString Description =
+            new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager,
+                typeof(Resources));
+
+        private const string Category = "Naming";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat,
+            Category, DiagnosticSeverity.Info, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get { return ImmutableArray.Create(Rule); }
+        }
+
+        public override void Initialize(AnalysisContext context)
+        {
+
+            //context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+            context.EnableConcurrentExecution();
+
+            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
+            // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
+            var namedTypeSymbol = (INamedTypeSymbol) context.Symbol;
+
+            if (!Util.IsVoType(namedTypeSymbol))
+            {
+                return;
+            }
+
+            var voTypeSyntax = namedTypeSymbol;
+
+            bool found = false;
+
+            foreach (ISymbol? memberDeclarationSyntax in voTypeSyntax.GetMembers("Validate"))
+            {
+                if (memberDeclarationSyntax is IMethodSymbol mds)
+                {
+                    if (!IsMethodStatic(mds))
+                    {
+                        continue;
+                    }
+
+                    if (mds.ReturnType.Name == "Validation")
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+            }
+
+            // Find just those named type symbols with names containing lowercase letters.
+            //if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
+            if (!found)
+            {
+                // For all such symbols, produce a diagnostic.
+                var diagnostic = Diagnostic.Create(
+                    descriptor: Rule,
+                    location: namedTypeSymbol.Locations[0],
+                    //effectiveSeverity: DiagnosticSeverity.Info, 
+                    additionalLocations: null,
+                    properties: null,
+                    namedTypeSymbol.Name);
+
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private static bool IsMethodStatic(IMethodSymbol mds) => mds.IsStatic;
+
+    }
+}
---
+++ tests/SmallTests/Analyzer1UnitTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Testing;
+using Vogen;
+using VerifyCS = Analyzer1.Test.CSharpCodeFixVerifier<Vogen.Analyzers.Analyzer1Analyzer, Vogen.CodeFixers.Analyzer1CodeFixProvider>;
+
+namespace Analyzer1.Test
+{
+    [TestClass]
+    public class Analyzer1UnitTest
+    {
+        //No diagnostics expected to show up
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var test = @"";
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        //Diagnostic and CodeFix both triggered and checked for
+        [TestMethod]
+        public async Task TestMethod2()
+        {
+            var input = @"
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Vogen;
+
+namespace ConsoleApplication1
+{
+    [ValueObject]
+    public partial class {|#0:TypeName|}
+    {   
+    }
+}
+";
+
+            var output = @"
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Vogen;
+
+namespace ConsoleApplication1
+{
+    [ValueObject]
+    public partial class TypeName
+    {
+        private static Validation Validate(int input)
+        {
+            return input > 0 ? Validation.Ok : Validation.Invalid(""!!"");
+        }
+    }
+}
+";
+
+            var expected =
+                VerifyCS.Diagnostic("Analyzer1").WithSeverity(DiagnosticSeverity.Info).WithLocation(0).WithArguments("TypeName");
+            
+            var loc = typeof(ValueObjectAttribute).Assembly.Location;
+
+            var referenceAssemblies = ReferenceAssemblies.Default
+                .AddAssemblies(
+                    ImmutableArray.Create("Vogen", "Vogen.SharedTypes", loc.Replace(".dll", string.Empty))
+                );
+            
+            var test = new VerifyCS.Test
+            {
+                TestState =
+                {
+                    Sources = {input },
+                    //ExpectedDiagnostics = {expected},
+                }, 
+
+                CompilerDiagnostics = CompilerDiagnostics.Suggestions,
+                //DisabledDiagnostics.Add() = new List<string>() { "CS1591"},
+                // DisabledDiagnostics = { "CS1591" },
+                //DiagnosticVerifier = (d,a,x) => d.Id == "Analyzer1",
+                ReferenceAssemblies = referenceAssemblies,
+                FixedCode = output,
+                ExpectedDiagnostics = { expected }
+            };
+
+
+            //test.TestState.ExpectedDiagnostics.Add(expected);
+            test.DisabledDiagnostics.Add("CS1591");
+            await test.RunAsync();
+            
+            //await VerifyCS.VerifyCodeFixAsync(test, expected, fixtest);
+        }
+    }
+}
