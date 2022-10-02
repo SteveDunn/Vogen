93add67baef8021d577dae5768804668bf5357e0
Steve Dunn <steve@dunnhq.com>
2022-10-02 08:45:13 +0100

    fix build

--- Build.ps1
+++ Build.ps1
@@ -30,7 +30,7 @@ exec { & dotnet clean -c Release }
 
 exec { & dotnet build -c Release }
 
-exec { & dotnet test -c Release -r $artifacts --no-build -l trx --l "GitHubActions;report-warnings=false" --verbosity=normal }
+exec { & dotnet test -c Release -r $artifacts --no-build -l trx -l "GitHubActions;report-warnings=false" --verbosity=normal }
 
 $samples = Get-ChildItem .\samples\Vogen.Examples.*
 
