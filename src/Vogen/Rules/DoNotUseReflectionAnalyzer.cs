730d1deb356cdf1a7088e0bb735d9795b71d4285
Steve Dunn <steve@dunnhq.com>
2022-10-07 07:09:36 +0100

    Tidy up

--- src/Vogen/Rules/DoNotUseReflectionAnalyzer.cs
+++ src/Vogen/Rules/DoNotUseReflectionAnalyzer.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Immutable;
+﻿using System.Collections.Immutable;
 using System.Linq;
-using Analyzer.Utilities.Extensions;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Operations;
 using Vogen.Diagnostics;
--- tests/Testbench/Program.cs
+++ tests/Testbench/Program.cs
@@ -6,6 +6,10 @@ using Vogen;
 // Console.WriteLine(x.Value);
 Console.WriteLine("Hello world!");
 
+var gt = new GenericThing<MyVo>();
+var vo = gt.GetNewItem();
+    Console.WriteLine(vo.Value);
+//gt.Set(default);
 
 //[ValueObject(typeof(int))]
 [ValueObject(typeof(int))]
@@ -13,4 +17,12 @@ public partial record MyVo
 {
     // public override string ToString() => "hello!";
 };
+
+public class GenericThing<T> where T : new()
+{
+    public T GetNewItem()
+    {
+        return new T();
+    }
+}
 
