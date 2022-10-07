cacaaa8ba5b078387a37621b44f6d0f93a24b1fe
Steve Dunn <steve@dunnhq.com>
2022-10-07 18:48:58 +0100

    Fix build by upping the version number

--- tests/LargeTests/ModuleInitializer.cs
+++ tests/LargeTests/ModuleInitializer.cs
@@ -16,6 +16,8 @@ public static class ModuleInitializer
                 "    [global::System.CodeDom.Compiler.GeneratedCodeAttribute(\"Vogen\", \"1.0.0.0\")]");
             s.Replace("    [global::System.CodeDom.Compiler.GeneratedCodeAttribute(\"Vogen\", \"2.0.0.0\")]",
                 "    [global::System.CodeDom.Compiler.GeneratedCodeAttribute(\"Vogen\", \"1.0.0.0\")]");
+            s.Replace("    [global::System.CodeDom.Compiler.GeneratedCodeAttribute(\"Vogen\", \"3.0.0.0\")]",
+                "    [global::System.CodeDom.Compiler.GeneratedCodeAttribute(\"Vogen\", \"1.0.0.0\")]");
         });
     }
 }
--- tests/MediumTests/ModuleInitializer.cs
+++ tests/MediumTests/ModuleInitializer.cs
@@ -17,6 +17,8 @@ public static class ModuleInitializer
                 "    [global::System.CodeDom.Compiler.GeneratedCodeAttribute(\"Vogen\", \"1.0.0.0\")]");
             s.Replace("    [global::System.CodeDom.Compiler.GeneratedCodeAttribute(\"Vogen\", \"2.0.0.0\")]",
                 "    [global::System.CodeDom.Compiler.GeneratedCodeAttribute(\"Vogen\", \"1.0.0.0\")]");
+            s.Replace("    [global::System.CodeDom.Compiler.GeneratedCodeAttribute(\"Vogen\", \"3.0.0.0\")]",
+                "    [global::System.CodeDom.Compiler.GeneratedCodeAttribute(\"Vogen\", \"1.0.0.0\")]");
         });
 
         SqlMapper.AddTypeHandler(new Vogen.IntegrationTests.TestTypes.ClassVos.DapperFooVo.DapperTypeHandler());
