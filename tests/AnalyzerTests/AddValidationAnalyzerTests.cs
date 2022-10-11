70d839c506ca1bb26912a4e7f1500c519f2f2dee
Steve Dunn <steve@dunnhq.com>
2022-10-11 05:53:08 +0100

    Updated Mocrosoft.CodeAnalysis.* to new (4.4.0-2.final) Before updating Microsoft.CodeAnalysis.CSharp.*.Testing

--- tests/AnalyzerTests/AddValidationAnalyzerTests.cs
+++ tests/AnalyzerTests/AddValidationAnalyzerTests.cs
@@ -66,7 +66,80 @@ namespace ConsoleApplication1
 
             var loc = typeof(ValueObjectAttribute).Assembly.Location;
 
-            var referenceAssemblies = ReferenceAssemblies.Default
+            // var referenceAssemblies = ReferenceAssemblies.Default
+            //     .AddAssemblies(
+            //         ImmutableArray.Create("Vogen", "Vogen.SharedTypes", loc.Replace(".dll", string.Empty))
+            var referenceAssemblies = ReferenceAssemblies.NetStandard.NetStandard20.AddAssemblies(
+                ImmutableArray.Create("Vogen", "Vogen.SharedTypes", loc.Replace(".dll", string.Empty)));
+
+            var test = new VerifyCS.Test
+            {
+                TestState =
+                {
+                    Sources = { input },
+                },
+
+                CompilerDiagnostics = CompilerDiagnostics.Suggestions,
+                ReferenceAssemblies = referenceAssemblies,
+                FixedCode = expectedOutput,
+                ExpectedDiagnostics = { expectedDiagnostic },
+            };
+
+            test.DisabledDiagnostics.Add("CS1591");
+
+            await test.RunAsync();
+        }
+
+
+#if NET7_0_OR_GREATER
+        [Fact]
+        public async Task Generic_CodeFixTriggeredForVoWithNoValidateMethod()
+        {
+            var input = LineEndingsHelper.Normalize(@"
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Vogen;
+
+namespace ConsoleApplication1
+{
+    [ValueObject<int>]
+    public partial class {|#0:TypeName|}
+    {   
+    }
+}");
+
+            var expectedOutput = LineEndingsHelper.Normalize(@"
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Vogen;
+
+namespace ConsoleApplication1
+{
+    [ValueObject<int>]
+    public partial class TypeName
+    {
+        private static Validation Validate(int input)
+        {
+            bool isValid = true; // todo: your validation
+            return isValid ? Validation.Ok : Validation.Invalid(""[todo: describe the validation]"");
+        }
+    }
+}");
+
+            var expectedDiagnostic =
+                VerifyCS.Diagnostic("AddValidationMethod").WithSeverity(DiagnosticSeverity.Info).WithLocation(0).WithArguments("TypeName");
+
+            var loc = typeof(ValueObjectAttribute).Assembly.Location;
+
+            var referenceAssemblies = ReferenceAssemblies.Net.Net60
                 .AddAssemblies(
                     ImmutableArray.Create("Vogen", "Vogen.SharedTypes", loc.Replace(".dll", string.Empty))
                 );
@@ -88,5 +161,7 @@ namespace ConsoleApplication1
 
             await test.RunAsync();
         }
+#endif
+
     }
 }
