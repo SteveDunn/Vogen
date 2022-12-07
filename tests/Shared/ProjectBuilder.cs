b68a76dfaf09e69951c85b0e716a0de5608e5d85
Steve Dunn <steve@dunnhq.com>
2022-12-07 15:04:50 +0000

    Fix tests

--- tests/Shared/ProjectBuilder.cs
+++ tests/Shared/ProjectBuilder.cs
@@ -51,7 +51,6 @@ namespace Shared
                     AddNuGetReference("Microsoft.NETCore.App.Ref", "3.1.0", "ref/netcoreapp3.1/");
                     AddNuGetReference("linq2db", "3.7.0", "lib/netcoreapp3.1/");
                     AddNuGetReference("Microsoft.EntityFrameworkCore", "5.0.17", "lib/netstandard2.1/");
-                    AddNuGetReference("System.Memory", "4.5.5", "lib/netstandard2.0");
                     AddNuGetReference("Dapper", "2.0.123", "lib/netstandard2.0/");
                     break;
 
@@ -71,7 +70,7 @@ namespace Shared
                     AddNuGetReference("Microsoft.EntityFrameworkCore", "5.0.17", "lib/netstandard2.1/");
                     AddNuGetReference("Dapper", "2.0.123", "lib/netstandard2.0/");
                     AddNuGetReference("System.Text.Json", "7.0.0", "lib/netstandard2.0/");
-                    AddNuGetReference("System.Memory", "4.5.5", "lib/netstandard2.0");
+                    AddNuGetReference("System.Memory", "4.5.5", "lib/netstandard2.0/");
                     break;
 
                 case TargetFramework.Net5_0:
