02c7764c19f1e79ea1039ec0e688c40e4c2f1415
Steve Dunn <steve@dunnhq.com>
2022-12-27 07:12:22 +0000

    Added tests for DateOnly

---
+++ tests/ConsumerTests/SerializationAndConversionTests/ClassVos/Types/DateOnlyVo.cs
@@ -0,0 +1,35 @@
+﻿#if NET6_0_OR_GREATER
+
+using System;
+
+namespace Vogen.IntegrationTests.TestTypes.ClassVos
+{
+    [ValueObject(conversions: Conversions.None, underlyingType: typeof(DateOnly))]
+    public partial class DateOnlyVo { }
+
+    [ValueObject(conversions: Conversions.None, underlyingType: typeof(DateOnly))]
+    public partial class NoConverterDateOnlyVo { }
+
+    [ValueObject(conversions: Conversions.TypeConverter, underlyingType: typeof(DateOnly))]
+    public partial class NoJsonDateOnlyVo { }
+
+    [ValueObject(conversions: Conversions.NewtonsoftJson, underlyingType: typeof(DateOnly))]
+    public partial class NewtonsoftJsonDateOnlyVo { }
+
+    [ValueObject(conversions: Conversions.SystemTextJson, underlyingType: typeof(DateOnly))]
+    public partial class SystemTextJsonDateOnlyVo { }
+
+    [ValueObject(conversions: Conversions.NewtonsoftJson | Conversions.SystemTextJson, underlyingType: typeof(DateOnly))]
+    public partial class BothJsonDateOnlyVo { }
+
+    [ValueObject(conversions: Conversions.EfCoreValueConverter, underlyingType: typeof(DateOnly))]
+    public partial class EfCoreDateOnlyVo { }
+
+    [ValueObject(conversions: Conversions.DapperTypeHandler, underlyingType: typeof(DateOnly))]
+    public partial class DapperDateOnlyVo { }
+
+    [ValueObject(conversions: Conversions.LinqToDbValueConverter, underlyingType: typeof(DateOnly))]
+    public partial class LinqToDbDateOnlyVo { }
+}
+
+#endif
