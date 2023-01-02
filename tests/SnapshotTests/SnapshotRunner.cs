793de78b5a8f0444eb56010e09b7e2c3f1413ddc
James Upjohn <jammehcow@jammehcow.co.nz>
2023-01-03 11:01:45 +1300

    fix(test): filter out target frameworks based on test project .NET version

--- tests/SnapshotTests/SnapshotRunner.cs
+++ tests/SnapshotTests/SnapshotRunner.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Immutable;
 using System.IO;
 using System.Runtime.CompilerServices;
@@ -71,7 +71,16 @@ namespace SnapshotTests
         public async Task RunOn(params TargetFramework[] frameworks)
         {
             _ = _source ?? throw new InvalidOperationException("No source!");
-            
+
+#if NET7_0
+            // Only run .NET 7 tests when using the .NET 7 SDK (prevents assembly versioning issues with <6.0)
+            frameworks = frameworks.Where(framework => framework == TargetFramework.Net7_0).ToArray();
+#elif NET6_0
+            // Alternatively, only run non-net7 tests when using the .NET 6 target
+            // as .NET 6 will use the .NET standard Vogen binary (without C#11 support)
+            frameworks = frameworks.Where(framework => framework != TargetFramework.Net7_0).ToArray();
+#endif
+
             foreach (var eachFramework in frameworks)
             {
                 _logger?.WriteLine($"Running on {eachFramework}");
