2bb20e9d0a63c7c64208e338c97fdf9ef237a14c
James Upjohn <jammehcow@jammehcow.co.nz>
2023-01-03 11:06:33 +1300

    fix(test): add missing using statement

--- tests/SnapshotTests/SnapshotRunner.cs
+++ tests/SnapshotTests/SnapshotRunner.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Immutable;
 using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using FluentAssertions;
