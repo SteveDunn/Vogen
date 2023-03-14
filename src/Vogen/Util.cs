57eefd5e8955d0e100b7d637b818f0e60231f468
Sir Phillip Tubell <ptubell@greenshades.com>
2023-03-14 08:35:26 -0400

    Added the Data property to validation so the exception thrown will contain that data.

--- src/Vogen/Util.cs
+++ src/Vogen/Util.cs
@@ -31,7 +31,9 @@ public static class Util
             return @$"var validation = {workItem.TypeToAugment.Identifier}.{workItem.ValidateMethod.Identifier.Value}(value);
             if (validation != Vogen.Validation.Ok)
             {{
-                throw new {workItem.ValidationExceptionFullName}(validation.ErrorMessage);
+                throw new {workItem.ValidationExceptionFullName}(validation.ErrorMessage) {{
+                    Data = validation.Data
+                }};
             }}
 ";
         return string.Empty;
