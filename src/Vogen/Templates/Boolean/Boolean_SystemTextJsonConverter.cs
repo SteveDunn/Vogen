5503b959a409facf2bbdd3733c17f5c0c3b4e3c0
Steve Dunn <steve@dunnhq.com>
2023-04-12 22:11:08 +0100

    Added ReadAsProperty/WriteAsProperty to STJ serializers

--- src/Vogen/Templates/Boolean/Boolean_SystemTextJsonConverter.cs
+++ src/Vogen/Templates/Boolean/Boolean_SystemTextJsonConverter.cs
@@ -10,4 +10,16 @@
             {
                 writer.WriteBooleanValue(value.Value);
             }
+
+#if NET6_0_OR_GREATER
+            public override VOTYPE ReadAsPropertyName(ref global::System.Text.Json.Utf8JsonReader reader, global::System.Type typeToConvert, global::System.Text.Json.JsonSerializerOptions options)
+            {
+                return VOTYPE.Deserialize(bool.Parse(reader.GetString()));
+            }
+
+            public override void WriteAsPropertyName(global::System.Text.Json.Utf8JsonWriter writer, VOTYPE value, global::System.Text.Json.JsonSerializerOptions options)
+            {
+                writer.WritePropertyName(value.Value.ToString());
+            }
+#endif            
         }
--- src/Vogen/Templates/DateTimeOffset/DateTimeOffset_SystemTextJsonConverter.cs
+++ src/Vogen/Templates/DateTimeOffset/DateTimeOffset_SystemTextJsonConverter.cs
@@ -10,4 +10,16 @@
             {
                 writer.WriteStringValue(value.Value);
             }
+
+#if NET6_0_OR_GREATER
+            public override VOTYPE ReadAsPropertyName(ref global::System.Text.Json.Utf8JsonReader reader, global::System.Type typeToConvert, global::System.Text.Json.JsonSerializerOptions options)
+            {
+                return VOTYPE.Deserialize(global::System.DateTimeOffset.ParseExact(reader.GetString(), "O", global::System.Globalization.CultureInfo.InvariantCulture));
+            }
+
+            public override void WriteAsPropertyName(System.Text.Json.Utf8JsonWriter writer, VOTYPE value, global::System.Text.Json.JsonSerializerOptions options)
+            {
+                writer.WritePropertyName(value.Value.ToString("O"));
+            }
+#endif            
         }
