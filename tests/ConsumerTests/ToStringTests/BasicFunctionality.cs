a6adfc0b71db31cb47473f11a03d6582bb053247
Steve Dunn <steve@dunnhq.com>
2023-05-08 20:28:18 +0100

    Add failing unit test

--- tests/ConsumerTests/ToStringTests/BasicFunctionality.cs
+++ tests/ConsumerTests/ToStringTests/BasicFunctionality.cs
@@ -4,10 +4,14 @@ namespace ConsumerTests.ToStringTests;
 
 public class BasicFunctionality
 {
-    // [Fact]
-    // public void ToString_uses_generated_method()
-    // {
-    // }
+    [Fact]
+    public void ToString_does_not_throw_for_something_uninitialized()
+    {
+#pragma warning disable VOG010
+        Age age = new Age();
+        age.ToString().Should().Be("[UNINITIALIZED]");
+#pragma warning restore VOG010
+    }
 
     [Fact]
     public void ToString_uses_generated_method()
