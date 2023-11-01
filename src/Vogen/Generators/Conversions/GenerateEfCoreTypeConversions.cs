0c3ad4cec3fc07834a0beab26d3310d424040cda
Steve Dunn <steve@dunnhq.com>
2023-11-01 22:24:38 +0000

    More EF Core changes to fix runtime errors re. nulls on value types

--- src/Vogen/Generators/Conversions/GenerateEfCoreTypeConversions.cs
+++ src/Vogen/Generators/Conversions/GenerateEfCoreTypeConversions.cs
@@ -36,7 +36,7 @@ internal class GenerateEfCoreTypeConversions : IGenerateConversion
                     
                                 public class EfCoreValueComparer : global::Microsoft.EntityFrameworkCore.ChangeTracking.ValueComparer<VOTYPE>
                                 {
-                                    public EfCoreValueComparer() : base((left, right) => (left == (VOTYPE)null && right == (VOTYPE)null) || (!left._isInitialized && !right._isInitialized) || (left._isInitialized && right._isInitialized && left.Equals(right)), instance => instance._isInitialized ? instance._value.GetHashCode() : 0) { }
+                                    public EfCoreValueComparer() : base((left, right) => (left == (VOTYPE)null && right == (VOTYPE)null) || object.ReferenceEquals(left, right) || (!left._isInitialized && !right._isInitialized) || (left._isInitialized && right._isInitialized && left.Equals(right)), instance => instance._isInitialized ? instance._value.GetHashCode() : 0) { }
                                 }
                     """;
         }
