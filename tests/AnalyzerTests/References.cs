04a2ab8ebd5c1912bd7a00b296162588962b89a3
Steve Dunn <steve@dunnhq.com>
2024-01-09 20:42:56 +0000

    Fix tests and add verified NET8.0 snapshots

--- tests/AnalyzerTests/References.cs
+++ tests/AnalyzerTests/References.cs
@@ -10,16 +10,6 @@ namespace AnalyzerTests
     {
         static readonly string _loc = typeof(ValueObjectAttribute).Assembly.Location;
 
-        // public static Lazy<ReferenceAssemblies> Net70AndOurs = new(() =>
-        //     new ReferenceAssemblies(
-        //             "net7.0",
-        //             new PackageIdentity(
-        //                 "Microsoft.NETCore.App.Ref",
-        //                 "7.0.0-rc.1.22426.10"),
-        //             Path.Combine("ref", "net7.0"))
-        //         .AddAssemblies(
-        //             ImmutableArray.Create("Vogen", "Vogen.SharedTypes", _loc.Replace(".dll", string.Empty))));
-
         public static Lazy<ReferenceAssemblies> Net80AndOurs = new(() =>
             new ReferenceAssemblies(
                     "net8.0",
