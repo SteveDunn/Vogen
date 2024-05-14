30b81efe95077f3f72ddf230fb1662e8fbb54cb5
Steve Dunn <steve@dunnhq.com>
2024-05-14 08:06:22 +0100

    feat: static abstract interface support - #511

--- tests/SnapshotTests/StaticAbstracts/StaticAbstractTests.cs
+++ tests/SnapshotTests/StaticAbstracts/StaticAbstractTests.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp;
 using Shared;
 using VerifyXunit;
 using Vogen;
@@ -8,62 +10,145 @@ namespace SnapshotTests.StaticAbstracts;
 [UsesVerify]
 public class StaticAbstractTests
 {
+    [UsesVerify]
+    public class Generates_nothing
+    {
+        [Fact]
+        public async Task when_run_on_less_than_csharp_11_even_when_specifying_it_should_generate()
+        {
+            var source = """
+                         using System;
+                         using Vogen;
+
+                         [assembly: VogenDefaults(
+                            systemTextJsonConverterFactoryGeneration: SystemTextJsonConverterFactoryGeneration.Omit, 
+                            conversions: Conversions.None, 
+                            staticAbstractsGeneration: StaticAbstractsGeneration.MostCommon)]
+
+                         [ValueObject(typeof(Guid))]
+                         public partial class MyVo { }
+                         """;
+
+            await new SnapshotRunner<ValueObjectGenerator>()
+                .WithSource(source)
+                .WithLanguageVersion(LanguageVersion.CSharp10)
+                .RunOn(TargetFramework.Net6_0);
+        }
+
+        [Fact]
+        public async Task if_omitted_in_global_config()
+        {
+            var source = """
+                         using System;
+                         using Vogen;
+
+                         [assembly: VogenDefaults(
+                            systemTextJsonConverterFactoryGeneration: SystemTextJsonConverterFactoryGeneration.Omit, 
+                            conversions: Conversions.None, 
+                            staticAbstractsGeneration: StaticAbstractsGeneration.Omit)]
+
+                         [ValueObject<Guid>]
+                         public partial class MyVo { }
+                         """;
+
+            await new SnapshotRunner<ValueObjectGenerator>()
+                .WithSource(source)
+                .RunOn(TargetFramework.Net8_0);
+        }
+
+        [Fact]
+        public async Task when_using_default_global_attribute()
+        {
+            var source = """
+                         using System;
+                         using Vogen;
+
+                         [ValueObject<Guid>]
+                         public partial class MyVo { }
+                         """;
+
+            await new SnapshotRunner<ValueObjectGenerator>()
+                .WithSource(source)
+                .RunOn(TargetFramework.Net8_0);
+        }
+    }
+    
     [Fact]
-    public async Task Generates_interface_definition_for_a_class_on_c_sharp_12()
+    public async Task when_using_implicit_operators()
     {
         var source = """
                      using System;
                      using Vogen;
 
-                     [ValueObject<Guid>]
+                     [assembly: VogenDefaults(
+                     systemTextJsonConverterFactoryGeneration: SystemTextJsonConverterFactoryGeneration.Omit, 
+                     conversions: Conversions.None, 
+                     staticAbstractsGeneration: StaticAbstractsGeneration.FactoryMethods | StaticAbstractsGeneration.EqualsOperators | StaticAbstractsGeneration.ImplicitCastFromPrimitive | StaticAbstractsGeneration.ImplicitCastToPrimitive)]
+
+                     [ValueObject<Guid>(toPrimitiveCasting: CastOperator.Implicit, fromPrimitiveCasting: CastOperator.Implicit)]
                      public partial class MyVo { }
                      """;
 
         await new SnapshotRunner<ValueObjectGenerator>()
             .WithSource(source)
-            .UseGeneratedInterfacesFile()
             .RunOn(TargetFramework.Net8_0);
     }
 
-    [Fact]
-    public async Task Generates_interface_definition_for_a_struct_on_c_sharp_12()
-    {
-        var source = """
-                     using System;
-                     using Vogen;
-
-                     [ValueObject<Guid>]
-                     public partial struct MyVo { }
-                     """;
-
-        await new SnapshotRunner<ValueObjectGenerator>()
-            .WithSource(source)
-            .UseGeneratedInterfacesFile()
-            .RunOn(TargetFramework.Net8_0);
-    }
 
     [Theory]
     [InlineData("struct")]
     [InlineData("class")]
     [InlineData("record struct")]
     [InlineData("record class")]
-    public async Task Generates_interface_definition_on_c_sharp_12(string type)
+    public async Task Generates_code_for_different_types(string type)
     {
         var source = $$"""
-
                        using System;
                        using Vogen;
 
-                       [assembly: VogenDefaults(customizations: Customizations.AddFactoryMethodForGuids)]
+                       [assembly: VogenDefaults(
+                       systemTextJsonConverterFactoryGeneration: SystemTextJsonConverterFactoryGeneration.Omit, 
+                       conversions: Conversions.None, 
+                       staticAbstractsGeneration: StaticAbstractsGeneration.MostCommon)]
                        
-                         [ValueObject<Guid>]
-                         public partial {{type}} MyVo { }
+                       [ValueObject<Guid>]
+                       public partial {{type}} MyVo { }
 
                        """;
 
         await new SnapshotRunner<ValueObjectGenerator>()
             .WithSource(source)
             .CustomizeSettings(s => s.UseFileName(TestHelper.ShortenForFilename(type)))
+            .RunOn(TargetFramework.Net8_0);
+    }
+
+    [Theory]
+    [InlineData("EqualsOperators")]
+    [InlineData("FactoryMethods")]
+    [InlineData("EqualsOperators", "FactoryMethods")]
+    [InlineData("InstanceMethodsAndProperties")]
+    [InlineData("InstanceMethodsAndProperties", "EqualsOperators", "FactoryMethods")]
+    [InlineData("InstanceMethodsAndProperties", "FactoryMethods", "EqualsOperators")]
+    public async Task Generates_code_for_different_variations(params string[] flags)
+    {
+        string attrs = string.Join(" | ", flags.Select(f => $"StaticAbstractsGeneration.{f}"));
+        var source = $$"""
+                       using System;
+                       using Vogen;
+
+                       [assembly: VogenDefaults(
+                       systemTextJsonConverterFactoryGeneration: SystemTextJsonConverterFactoryGeneration.Omit, 
+                       conversions: Conversions.None, 
+                       staticAbstractsGeneration: {{attrs}})]
+                       
+                       [ValueObject<Guid>]
+                       public partial struct MyVo { }
+
+                       """;
+
+        await new SnapshotRunner<ValueObjectGenerator>()
+            .WithSource(source)
+            .CustomizeSettings(s => s.UseFileName(TestHelper.ShortenForFilename(attrs)))
             .RunOn(TargetFramework.Net8_0);
     }
 }
