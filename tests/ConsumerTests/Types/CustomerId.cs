7c78e9d44e36d88c6fbd66702d451d3415a71af6
Steve Dunn <steve@dunnhq.com>
2024-05-26 08:03:53 +0100

    Fix build

--- tests/ConsumerTests/Types/CustomerId.cs
+++ tests/ConsumerTests/Types/CustomerId.cs
@@ -1,15 +1,4 @@
 ﻿namespace Vogen.Tests.Types;
 
-[ValueObject<float>]
-public readonly partial struct CustomerId {
-    private static Validation Validate(float input)
-    {
-        bool isValid = true; // todo: your validation
-        return isValid ? Validation.Ok : Validation.Invalid("[todo: describe the validation]");
-    }
-    private static float NormalizeInput(string input)
-    {
-        // todo: normalize (sanitize) your input;
-        return 1f;
-    }
-}
+[ValueObject<int>]
+public readonly partial struct CustomerId;
