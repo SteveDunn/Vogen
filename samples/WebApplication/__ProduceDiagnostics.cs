cae2ca9f6676707c53f3920d5c391c6c55fdd28c
Steve Dunn <steve@dunnhq.com>
2024-05-28 11:50:56 +0100

    Added snapshot tests

---
+++ samples/WebApplication/__ProduceDiagnostics.cs
@@ -0,0 +1,9 @@
+using Vogen;
+
+// If Vogen find this type in this namespace, it'll generate a `diganostics.cs`, which is just a
+// C# file with a multi-line comment describing what language version, targets, config etc.
+
+namespace Vogen;
+
+// ReSharper disable once UnusedType.Global
+public class __ProduceDiagnostics { }
