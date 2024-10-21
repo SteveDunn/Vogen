88865893beb66ef5a4b1df412eb78ff72b061f6f
Steve Dunn <steve@dunnhq.com>
2024-10-21 10:37:44 +0100

    Tidy up

--- src/Vogen/Rules/DoNotCompareWithPrimitivesInEfCoreAnalyzer.cs
+++ src/Vogen/Rules/DoNotCompareWithPrimitivesInEfCoreAnalyzer.cs
@@ -35,74 +35,8 @@ public class DoNotCompareWithPrimitivesInEfCoreAnalyzer : DiagnosticAnalyzer
         context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
         context.EnableConcurrentExecution();
 
-        context.RegisterSyntaxNodeAction(AnalyzeSyntaxNode, SyntaxKind.EqualsExpression);
         context.RegisterSyntaxNodeAction(AnalyzeInvocation, SyntaxKind.InvocationExpression);
     }
-
-    private void AnalyzeSyntaxNode(SyntaxNodeAnalysisContext context)
-    {
-        var equalsExpression = (BinaryExpressionSyntax) context.Node;
-
-        // Check if the expression is within a Where clause
-        if (!(equalsExpression.Parent is LambdaExpressionSyntax lambdaParent)) return;
-        if (!(lambdaParent.Parent is ArgumentSyntax argParent)) return;
-        if (!(argParent.Parent is ArgumentListSyntax argListParent)) return;
-        if (!(argListParent.Parent is InvocationExpressionSyntax invocationParent)) return;
-
-        // Ensure it is the correct method
-        var methodSymbol = context.SemanticModel.GetSymbolInfo(invocationParent).Symbol as IMethodSymbol;
-        if (methodSymbol is null) return;
-
-        // Get the left and right hand sides
-        var left = context.SemanticModel.GetTypeInfo(equalsExpression.Left).Type!;
-        var right = context.SemanticModel.GetTypeInfo(equalsExpression.Right).Type!;
-
-        if (!IsEfCore(methodSymbol)) return;
-
-        // Check if left is ValueObject and right is integer
-        if (IsValueObject(left) && right.SpecialType == SpecialType.System_Int32)
-        {
-            context.ReportDiagnostic(DiagnosticsCatalogue.BuildDiagnostic(_rule, left.Name, equalsExpression.GetLocation()));
-
-            //var diagnostic = Diagnostic.Create(_rule, equalsExpression.GetLocation());
-            //context.ReportDiagnostic(diagnostic);
-        }
-
-
-        bool IsEfCore(IMethodSymbol methodSymbol)
-        {
-            if (methodSymbol.Name != "Where") return false;
-
-            if (methodSymbol.ReceiverType is not INamedTypeSymbol namedTypeSymbol)
-            {
-                return false;
-            }
-
-            // Check if the type is from the EF Core namespace
-            INamespaceSymbol? ns = namedTypeSymbol.ContainingNamespace;
-            if (ns is null) return false;
-
-            foreach (var interfaceType in namedTypeSymbol.AllInterfaces)
-            {
-                if (interfaceType.ConstructedFrom.ToString() == "System.Linq.IQueryable")
-                {
-                    // Find the Provider property
-                    foreach (var member in interfaceType.GetMembers())
-                    {
-                        if (member is IPropertySymbol propertySymbol && propertySymbol.Name == "Provider")
-                        {
-                            return false;
-                        }
-                    }
-                }
-            }
-
-            return false;
-        }
-    }
-
-    private static bool IsValueObject(ITypeSymbol type) => 
-        type is INamedTypeSymbol symbol && VoFilter.IsTarget(symbol);
 
     private static void AnalyzeInvocation(SyntaxNodeAnalysisContext context)
     {
@@ -114,35 +48,40 @@ public class DoNotCompareWithPrimitivesInEfCoreAnalyzer : DiagnosticAnalyzer
             return;
         }
 
-        var symbolInfo = context.SemanticModel.GetSymbolInfo(memberAccessExpr.Expression);
-        if (symbolInfo.Symbol is not IPropertySymbol ps) return;
+        if (!IsAMemberOfDbSet(context, memberAccessExpr)) return;
 
-        var dbSetType = context.SemanticModel.Compilation.GetTypeByMetadataName("Microsoft.EntityFrameworkCore.DbSet`1");
-        if (dbSetType is null) return;
-
-        if (!InheritsFrom(ps.Type, dbSetType)) return;
-
-        foreach (var lambdaExpression in invocationExpr.ArgumentList.Arguments.Where(e => e.Expression is LambdaExpressionSyntax))
+        foreach (ArgumentSyntax eachArgument in invocationExpr.ArgumentList.Arguments.Where(e => e.Expression is LambdaExpressionSyntax))
         {
-            foreach (var binaryExpression in lambdaExpression.DescendantNodes().OfType<BinaryExpressionSyntax>())
+            foreach (BinaryExpressionSyntax eachBinaryExpression in eachArgument.DescendantNodes().OfType<BinaryExpressionSyntax>())
             {
-                ITypeSymbol? left = context.SemanticModel.GetTypeInfo(binaryExpression.Left).Type;
-                ITypeSymbol? right = context.SemanticModel.GetTypeInfo(binaryExpression.Right).Type;
+                ITypeSymbol? left = context.SemanticModel.GetTypeInfo(eachBinaryExpression.Left).Type;
+                ITypeSymbol? right = context.SemanticModel.GetTypeInfo(eachBinaryExpression.Right).Type;
 
                 if (left is null || right is null) continue;
 
                 // Check if left is ValueObject and right is integer
                 if (IsValueObject(left) && right.SpecialType == SpecialType.System_Int32)
                 {
-                    context.ReportDiagnostic(DiagnosticsCatalogue.BuildDiagnostic(_rule, left.Name, binaryExpression.GetLocation()));
+                    context.ReportDiagnostic(DiagnosticsCatalogue.BuildDiagnostic(_rule, left.Name, eachBinaryExpression.GetLocation()));
                 }
             }
         }
-
-
-        // var diagnostic = Diagnostic.Create(_rule, memberAccessExpr.GetLocation(), ps.Name);
-        // context.ReportDiagnostic(diagnostic);
     }
+
+    private static bool IsAMemberOfDbSet(SyntaxNodeAnalysisContext context, MemberAccessExpressionSyntax memberAccessExpr)
+    {
+        var symbolInfo = context.SemanticModel.GetSymbolInfo(memberAccessExpr.Expression);
+        if (symbolInfo.Symbol is not IPropertySymbol ps) return false;
+
+        var dbSetType = context.SemanticModel.Compilation.GetTypeByMetadataName("Microsoft.EntityFrameworkCore.DbSet`1");
+        
+        if (dbSetType is null) return false;
+
+        return InheritsFrom(ps.Type, dbSetType);
+    }
+
+    private static bool IsValueObject(ITypeSymbol type) => 
+        type is INamedTypeSymbol symbol && VoFilter.IsTarget(symbol);
 
     private static bool InheritsFrom(ITypeSymbol type, INamedTypeSymbol baseType)
     {
