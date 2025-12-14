6846f04fd8fa518f9aec16a08d78e52fc1fe5ae6
Steve Dunn <steve@dunnhq.com>
2025-12-14 07:49:55 +0000

    more simples

--- tests/ConsumerTests/a.cs
+++ tests/ConsumerTests/a.cs
@@ -1,42 +1,23 @@
 ﻿namespace NuGet
 {
-    public class PackageIdentity;
-    public class LicenseMetadata;
-    public class PackageDeprecationMetadata;
-    public class PackageVulnerabilityMetadata;
+    public class Type1;
+    public class Type2;
 }
 
 namespace ConsumerTests
 {
-    using PackageIdentity = global::NuGet.PackageIdentity;
-    using LicenseMetadata = global::NuGet.LicenseMetadata;
-    using PackageDeprecationMetadata = global::NuGet.PackageDeprecationMetadata;
-    using PackageVulnerabilityMetadata = global::NuGet.PackageVulnerabilityMetadata;
+    using PackageIdentity = global::NuGet.Type1;
+    using Type2 = global::NuGet.Type2;
     using Vogen;
 
     public class NuGet
     {
     }
 
-    [ValueObject<ValueTuple<PackageIdentity, LicenseMetadata, Uri, PackageDeprecationMetadata, PackageVulnerabilityMetadata>>]
+    [ValueObject<ValueTuple<PackageIdentity, Type2>>]
     internal partial record class PackageMetadata
     {
-        public static PackageMetadata NuGet => From((null!, null!, null!, null!, null!));
+        public static PackageMetadata NuGet => From((new PackageIdentity(),new Type2()));
         public PackageIdentity Identity => Value.Item1;
-
-        public LicenseMetadata License => Value.Item2;
-
-        public Uri LicenseUrl => Value.Item3;
-
-        public PackageDeprecationMetadata DeprecationMetadata => Value.Item4;
-
-        public PackageVulnerabilityMetadata VulnerabilityMetadata => Value.Item5;
-
-        private static Validation Validate(
-            (PackageIdentity Identity, LicenseMetadata LicenseMetadata, Uri LicenseUrl, PackageDeprecationMetadata DeprecationMetadata,
-                PackageVulnerabilityMetadata VulnerabilityMetadata) input)
-            => input.Identity != null && input.LicenseMetadata != null
-                ? Validation.Ok
-                : Validation.Invalid($"'{input}' is not a valid {nameof(PackageMetadata)}.");
     }
 }
