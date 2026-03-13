8d22d6fab757479dcf2c64e9a81a09bf90206eec
Steve Dunn <steve@dunnhq.com>
2026-03-13 08:09:03 +0000

    add: example project showcasing extensions for nullable value object creation

---
+++ samples/ExampleExtensions/Program.cs
@@ -0,0 +1,33 @@
+﻿using Vogen;
+
+[assembly: VogenDefaults(
+    staticAbstractsGeneration: StaticAbstractsGeneration.MostCommon)]
+
+Console.WriteLine("Hello, World!");
+
+int supplied = 123;
+int? notSupplied = null;
+
+var non_null1 = MyId.From(supplied);
+var non_null2 = MyId.FromNullable(supplied);
+MyId? null_1 = MyId.FromNullable(notSupplied);
+
+Write(non_null1);
+Write(non_null2);
+Write(null_1);
+return;
+
+static void Write(MyId? vo) => Console.WriteLine(vo?.Value.ToString() ?? "null");
+
+
+[ValueObject<int>]
+public partial class MyId;
+
+static class Extensions
+{
+    extension<TWrapper, TPrimitive>(IVogen<TWrapper, TPrimitive>) where TWrapper : IVogen<TWrapper, TPrimitive> where TPrimitive : struct
+    {
+        public static TWrapper? FromNullable(TPrimitive? value) => value is null ? default : TWrapper.From(value.Value);
+        
+    }
+}
