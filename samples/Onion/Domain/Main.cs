9112ec1be69b9a11f5a6cf467df7ae9d115ca63c
Steve Dunn <steve@dunnhq.com>
2026-07-21 06:45:54 +0100

    Fix Vogen.SharedTypes versioning

--- samples/Onion/Domain/Main.cs
+++ samples/Onion/Domain/Main.cs
@@ -3,8 +3,11 @@
 // We don't need to emit the System.Text.Json converter factory because
 // System.Text.Json, in the Infra project (or anything that references this)
 // will have access to the 'fully formed' value objects.
+// We also emit the static abstract interface, IVogen. Projects that reference this should use the 'Omit' option, otherwise they will get
+// a compiler error because of duplicate definitions.
 [assembly: VogenDefaults(
-    systemTextJsonConverterFactoryGeneration: SystemTextJsonConverterFactoryGeneration.Omit, 
+    systemTextJsonConverterFactoryGeneration: SystemTextJsonConverterFactoryGeneration.Omit,
+    staticAbstractsGeneration: StaticAbstractsGeneration.ValueObjectsDeriveFromTheInterface,
     conversions: Conversions.SystemTextJson | 
                  Conversions.TypeConverter)]
 
